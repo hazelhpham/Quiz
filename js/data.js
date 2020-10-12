f310b472e6394eddc89962003e1af723dcc205d3
Hazel H Pham <phd.h99@gmail.com>
2020-10-12 19:56:52 +0700

    Update data.js

---
+++ js/data.js
@@ -0,0 +1,156 @@
+const Quiz = {
+  math: [
+    {
+      question: "what is the product of 0 and 1 ? ",
+      choiceA: 0,
+      choiceB: 100,
+      choiceC: 3,
+      choiceD: 9,
+    },
+    {
+      question: "the least number of 2 digits is ? ",
+      choiceA: 99,
+      choiceB: 11,
+      choiceC: 90,
+      choiceD: 10,
+    },
+    {
+      question: "what is the biggest number ? ",
+      choiceA: 9999999,
+      choiceB: -1000000,
+      choiceC: 3,
+      choiceD: 4383,
+    },
+    {
+      question: "what is the smallest number ? ",
+      choiceA: -9,
+      choiceB: -11,
+      choiceC: 0,
+      choiceD: 100,
+    },
+    {
+      question:
+        "There are 3 candies in the pocket. Andie eats 2 candies. How many candies are there left in the pocket?  ",
+      choiceA: 1,
+      choiceB: 0,
+      choiceC: 5,
+      choiceD: 3,
+    },
+  ],
+  geography: [
+    {
+      question: "What is the capital of Australia ?",
+      choiceA: "Canberra",
+      choiceB: "Sydney",
+      choiceC: "Melbourne",
+      choiceD: "None of the above",
+      //answer: A
+    },
+    {
+      question: "What is the largest continent? ",
+      choiceA: "Asia",
+      choiceB: "Europe",
+      choiceC: "Africa",
+      choiceD: "Australia",
+      //answer: A
+    },
+    {
+      question: "Which is the second largest country? ",
+      choiceA: "Australia",
+      choiceB: "Russia",
+      choiceC: "Canada",
+      choiceD: "China",
+      //answer: C
+    },
+    {
+      question: "Which country has the smallest population? ",
+      choiceA: "Vietnam",
+      choiceB: "Singapore",
+      choiceC: "Vatican",
+      choiceD: "Bangladesh",
+      //answer: C
+    },
+    {
+      question: "Berlin is the capital of which country?  ",
+      choiceA: "France",
+      choiceB: "Singapore",
+      choiceC: "Italy",
+      choiceD: "Germany",
+      //answer: D
+    },
+  ],
+  music: [
+    {
+      question: "Berlin is the capital of which country?  ",
+      choiceA: "France",
+      choiceB: "Singapore",
+      choiceC: "Italy",
+      choiceD: "Germany",
+      //answer: D
+    },
+    {
+      question: "Berlin is the capital of which country?  ",
+      choiceA: "France",
+      choiceB: "Singapore",
+      choiceC: "Italy",
+      choiceD: "Germany",
+      //answer: D
+    },
+    {
+      question: "Berlin is the capital of which country?  ",
+      choiceA: "France",
+      choiceB: "Singapore",
+      choiceC: "Italy",
+      choiceD: "Germany",
+      //answer: D
+    },
+    {
+      question: "Berlin is the capital of which country?  ",
+      choiceA: "France",
+      choiceB: "Singapore",
+      choiceC: "Italy",
+      choiceD: "Germany",
+      //answer: D
+    },
+    {
+      question: "Berlin is the capital of which country?  ",
+      choiceA: "France",
+      choiceB: "Singapore",
+      choiceC: "Italy",
+      choiceD: "Germany",
+      //answer: D
+    },
+  ],
+  films: [
+    {
+        question: "The main actor in Titanic is: ",
+        choiceA:"Leonardo DiCaprio",
+        choiceB:"Brad Pitt",
+        choiceC:"Adam Levine",
+        choiceD:"Justin Bieber",
+        //answer: A
+
+
+    },
+    {
+        question: "How many chapters does Harry Potter film have? ",
+        choiceA:1,
+        choiceB: 3,
+        choiceC:7,
+        choiceD:8,
+        //answer: C
+
+    },
+    {
+        question: "In the movie Home Alone, where did Kevin get lost in?",
+        choiceA:"New York",
+        choiceB:"New Jersey",
+        choiceC:"Miami",
+        choiceD:"Chicago",
+        //answer: A
+
+    },
+  ],
+  sports: [],
+  general_knowledge: [],
+};
