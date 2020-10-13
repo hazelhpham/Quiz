c9aa1dec8681fb318f32235c95f32e9aa138df7d
Hazel H Pham <phd.h99@gmail.com>
2020-10-14 02:13:02 +0700

    Update math.js

--- js/math.js
+++ js/math.js
@@ -1,5 +1,6 @@
-let choices = [];
-let nameOfChoices = ["A", "B", "C", "D"];
+
+
+//Database of the Math quiz
 const listOfQuiz = {
   math: [
     {
@@ -13,7 +14,7 @@ const listOfQuiz = {
       answer: "D",
     },
     {
-      question: "what is the biggest number ? ",
+      question: "What is the biggest number ? ",
       choices: [9999999, -1000000, 3, 4383],
       answer: "A",
     },
@@ -31,10 +32,10 @@ const listOfQuiz = {
   ],
 };
 
-
 let numQuestions = [1, 2, 3, 4, 5];
 let numChoices = listOfQuiz.math[0].choices.length;
 
+//function to shuffle the questions
 function shuffleArray(array) {
   for (var i = array.length - 1; i > 0; i--) {
     // Generate random number
@@ -50,7 +51,11 @@ function shuffleArray(array) {
 
 let random = shuffleArray(numQuestions);
 
-
+let nameOfChoices = ["A", "B", "C", "D"];
+let count = 0;
+document.getElementById("submit").onclick = function () {
+  submitFunction();
+};
 for (let i = 0; i < numQuestions.length; i++) {
   let r = random[i];
   document.getElementById("question").innerHTML =
@@ -63,8 +68,15 @@ for (let i = 0; i < numQuestions.length; i++) {
     nameOfChoices[2] + ". " + listOfQuiz.math[r - 1].choices[2];
   document.getElementById("choiceD").innerHTML =
     nameOfChoices[3] + ". " + listOfQuiz.math[r - 1].choices[3];
+
+  function submitFunction() {
+    let userAns = document.getElementById("answer").value;
+    let ans = listOfQuiz.math[r - 1].answer;
+    if (userAns === ans ) {
+      alert("Correct answer!");
+      count = count + 1;
+    } else {
+      alert("Incorrect answer!");
+    }
+  }
 }
-
-//-------------------------end randomize question---------------------------
-
-
